0c4c809b00cef31b5fc2166781f0f1f13e90debd
wzd <wzdxx1314@163.com>
2017-07-07 16:58:13 +0800

    extend demo

--- qwe.js
+++ qwe.js
@@ -1,11 +1,51 @@
-;(function(global,callBack){
-    callBack(global,undefined);
-})(window || this ,function(global){
-    var qwe = function(){};
-    qwe.prototype={
-        //解决多层嵌套对象的合并问题
-        extend:function(){},
-        //检查对象之间的差异，两个对象是否在相同熟悉上面的值
-        diff:function(){}
+;(function (global, callBack) {
+    callBack(global, undefined);
+})(window || this, function (global) {
+    var qwe = {};
+    //extend是合并两个对象中相同的部分，然后添加各不相同的部分，所添加的类型包括对象 ，数组 ，string ，number，函数不再此列
+    qwe.extend = function () {
+        var origin = {}
+        for (var i = 0; i < arguments.length; i++) {
+
+        }
+
     }
+
 })
+function add(a, b) {
+    if (b instanceof Array) {
+        a = {}
+        for (var pro in b) {
+            if (b[pro] instanceof Array) {
+                a[pro] = [];
+                add(a[pro], b[pro]);
+            }
+            else if (b[pro] instanceof Object) {
+                a[pro] = {}
+                add(a[pro], b[pro]);
+            }
+            else {
+                a[pro] = b[pro]
+            }
+        }
+    }
+    else if (b instanceof Array) {
+        a = [];
+        for (var i = 0; i < b.length; i++) {
+            if (b[i] instanceof Array) {
+                a[i] = [];
+                add(a[i], b[i]);
+            }
+            else if (b[i] instanceof Object) {
+                a[i] = {};
+                add(a[i], b[i]);
+            }
+            else{
+               a[i] =b[i];
+            }
+        }
+    }
+    else{
+        a = b;
+    }
+}
