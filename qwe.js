37375c0188cff9e540cfbefe1f98d2a1c21ffe2a
wzd <wzdxx1314@163.com>
2017-07-08 13:27:22 +0800

    object拓展功能

--- qwe.js
+++ qwe.js
@@ -1,51 +1,25 @@
 ;(function (global, callBack) {
     callBack(global, undefined);
 })(window || this, function (global) {
+    function _extend(origin, later) {
+        //对于每个later对象属性是否origin有
+        for( prop in later){
+            if(Object.prototype.hasOwnProperty.call(origin,prop) && later[prop] instanceof Object){
+                _extend(origin[prop],later[prop])
+            }
+            else{
+                origin[prop]  = JSON.parse(JSON.stringify(later[prop]))
+            }
+        }
+    }
     var qwe = {};
     //extend是合并两个对象中相同的部分，然后添加各不相同的部分，所添加的类型包括对象 ，数组 ，string ，number，函数不再此列
     qwe.extend = function () {
         var origin = {}
-        for (var i = 0; i < arguments.length; i++) {
-
-        }
-
+        arguments.forEach(function(obj){
+            _extend(origin,obj);
+        })
+        return origin;
     }
 
 })
-function add(a, b) {
-    if (b instanceof Array) {
-        a = {}
-        for (var pro in b) {
-            if (b[pro] instanceof Array) {
-                a[pro] = [];
-                add(a[pro], b[pro]);
-            }
-            else if (b[pro] instanceof Object) {
-                a[pro] = {}
-                add(a[pro], b[pro]);
-            }
-            else {
-                a[pro] = b[pro]
-            }
-        }
-    }
-    else if (b instanceof Array) {
-        a = [];
-        for (var i = 0; i < b.length; i++) {
-            if (b[i] instanceof Array) {
-                a[i] = [];
-                add(a[i], b[i]);
-            }
-            else if (b[i] instanceof Object) {
-                a[i] = {};
-                add(a[i], b[i]);
-            }
-            else{
-               a[i] =b[i];
-            }
-        }
-    }
-    else{
-        a = b;
-    }
-}
